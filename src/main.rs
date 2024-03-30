cd50bf8497f3f9ed031f5a46ee2f3fb9d4ae12cc
mwiese <mwiese@match-trade.com>
2024-03-30 21:31:30 +0100

    play test sound using rodio crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+use std::io::BufReader;
+
+use rodio::{Decoder, OutputStream, Source};
+
 fn main() {
-    println!("Hello, world!");
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let file = BufReader::new(File::open("sounds/warning.wav").unwrap());
+    let source = Decoder::new(file).unwrap();
+    let _ = stream_handle.play_raw(source.convert_samples());
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
