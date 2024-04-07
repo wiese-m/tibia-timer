3bee41d066632ecaae756ca2e29f484228684158
mwiese <mwiese@match-trade.com>
2024-04-07 20:22:19 +0200

    improve ui with adjustable timer values

--- src/main.rs
+++ src/main.rs
@@ -9,37 +9,35 @@ slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-    let countdown = ui.get_countdown();
 
-    ui.on_start_timer(move || {
+    ui.on_start_timer(move |countdown, warning_threshold, critical_threshold| {
+        let countdown: i32 = countdown.trim().parse().unwrap();
+        let warning_threshold: i32 = warning_threshold.trim().parse().unwrap();
+        let critical_threshold: i32 = critical_threshold.trim().parse().unwrap();
         std::thread::spawn(move || {
-            start_countdown(countdown);
+            start_countdown(countdown, warning_threshold, critical_threshold);
         });
     });
 
     ui.run()
 }
 
-fn start_countdown(mut countdown: i32) {
+fn start_countdown(countdown: i32, warning_threshold: i32, critical_threshold: i32) {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-
-    // let mut countdown = 15;
-    let warning_threshold = 10;
-    let critical_threshold = 5;
-    
-    while countdown > 0 {
-        if countdown <= warning_threshold {
-            let warning_file = BufReader::new(File::open("sounds/warning.wav").unwrap());
-            let warning_source = Decoder::new(warning_file).unwrap();
-            let _ = stream_handle.play_raw(warning_source.convert_samples());
+    loop {
+        for t in (1..countdown+1).rev() {
+            if t <= warning_threshold {
+                let warning_file = BufReader::new(File::open("sounds/warning.wav").unwrap());
+                let warning_source = Decoder::new(warning_file).unwrap();
+                let _ = stream_handle.play_raw(warning_source.convert_samples());
+            }
+            if t <= critical_threshold {
+                let critical_file = BufReader::new(File::open("sounds/critical.mp3").unwrap());
+                let critical_source = Decoder::new(critical_file).unwrap();
+                let _ = stream_handle.play_raw(critical_source.convert_samples());
+            }
+            println!("Countdown: {}", t);
+            sleep(Duration::from_secs(1));
         }
-        if countdown <= critical_threshold {
-            let critical_file = BufReader::new(File::open("sounds/critical.mp3").unwrap());
-            let critical_source = Decoder::new(critical_file).unwrap();
-            let _ = stream_handle.play_raw(critical_source.convert_samples());
-        }
-        println!("Countdown: {}", countdown);
-        countdown -= 1;
-        sleep(Duration::from_secs(1));
     }
 }
